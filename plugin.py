38f6f768c7429e5bfeabfab616f00f3d31b75850
wallentx <william.allentx@gmail.com>
2024-06-12 14:25:38 -0500

    Fixing plugin.py

--- plugin.py
+++ plugin.py
@@ -1,8 +1,8 @@
 from __future__ import annotations
 
 import os
-from lsp_utils import NpmClientHandler
-
+from lsp_utils import NpmClientHandler, ClientConfig
+from sublime_plugin import WindowCommand
 
 def plugin_loaded() -> None:
     LspActionsPlugin.setup()
@@ -25,7 +25,17 @@ class LspActionsPlugin(NpmClientHandler):
     )
 
     @classmethod
-    def on_pre_start(cls, window: sublime.Window, initiating_view: sublime.View, workspace_folders: List[Dict[str, Any]], configuration: ClientConfig) -> Optional[ClientConfig]:
+    def on_pre_start(
+        cls,
+        window: sublime.Window,
+        initiating_view: sublime.View,
+        workspace_folders: List[Dict[str, Any]],
+        configuration: ClientConfig
+    ) -> Optional[ClientConfig]:
+        if not configuration:
+            return None
+
         configuration.command = ["node", cls.server_binary_path, "--stdio"]
         print("Executing command:", configuration.command)
+        print("Server binary path:", cls.server_binary_path)
         return configuration
