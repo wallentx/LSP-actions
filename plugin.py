1c248651d6c109e7416057954cf8d967088c42fd
wallentx <william.allentx@gmail.com>
2024-06-13 02:27:39 -0500

    Updating plugin.py

--- plugin.py
+++ plugin.py
@@ -3,25 +3,21 @@ from __future__ import annotations
 import os
 from lsp_utils import NpmClientHandler, ClientConfig
 from sublime_plugin import WindowCommand
+from typing import Optional, Dict, Any, List
 
 def plugin_loaded() -> None:
     LspActionsPlugin.setup()
 
-
 def plugin_unloaded() -> None:
     LspActionsPlugin.cleanup()
-
 
 class LspActionsPlugin(NpmClientHandler):
     package_name = __package__
     server_directory = "language-server"
     server_binary_path = os.path.join(
         server_directory,
-        "node_modules",
-        "@actions",
-        "languageserver",
         "dist",
-        "index.js"
+        "cli.js"
     )
 
     @classmethod
@@ -35,7 +31,7 @@ class LspActionsPlugin(NpmClientHandler):
         if not configuration:
             return None
 
-        configuration.command = ["node", cls.server_binary_path, "--stdio"]
+        configuration.command = ["npx", "tsx", cls.server_binary_path, "--stdio"]
         print("Executing command:", configuration.command)
         print("Server binary path:", cls.server_binary_path)
         return configuration
